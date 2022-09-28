bdfb879139fe52d6a9c5e5e5fff38a3d98cdce03
Yuriy <xstream_06@mail.ru>
2022-09-29 00:27:54 +0300

    HW_1 Update

--- src/user.ts
+++ src/user.ts
@@ -1,18 +1,19 @@
 import { renderBlock } from './lib.js'
 
-export function renderUserBlock (favoriteItemsAmount) {
-  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
-  const hasFavoriteItems = favoriteItemsAmount ? true : false
+export function renderUserBlock (name:string, avatarLink:string, favoriteItemsAmount: number) {
+  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 0;
+  const hasFavoriteItems = favoriteItemsAmount ? true : false;
+  const items: number | string = favoritesCaption > 0 ? favoritesCaption : 'ничего нет';
 
   renderBlock(
     'user-block',
     `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <img class="avatar" src="${avatarLink}" alt="Wade Warren" />
       <div class="info">
-          <p class="name">Wade Warren</p>
+          <p class="name">${name}</p>
           <p class="fav">
-            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${items}
           </p>
       </div>
     </div>
